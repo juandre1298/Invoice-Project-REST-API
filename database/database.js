6e2234cca013d2941228c10c13d570e3e382b67a
Juan Salas <juandre1298@gmail.com>
2023-06-07 17:01:58 -0500

    update

--- database/database.js
+++ database/database.js
@@ -5,6 +5,7 @@ import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from "../config.js";
   host: "localhost",
   dialect: "mysql",
 }); */
+
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
